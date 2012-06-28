a6f27835888ba5ae72e017b95817f0798ec0518e
andrei.yerameyeu <Andrei_Yerameyeu@gmail.com>
2012-06-28 16:08:29 +0300

    change Exercise entity bean

--- training_core/src/org/musclemen/model/training/Exercise.java
+++ training_core/src/org/musclemen/model/training/Exercise.java
@@ -1,6 +1,26 @@
 package org.musclemen.model.training;
 
 public class Exercise {
-	//jjj
+	
+	/* Keeps the name of exercise */
+	private String name;
+
+	/**
+	 * Gets a name. 
+	 * 
+	 * @return
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Sets a name.
+	 * 
+	 * @param name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
