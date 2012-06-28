8b09c841c9cd4bc33b3a9a932648bbff31b913f7
siarheiyelin <siarheiyelin85@gmail.com>
2012-06-28 07:58:08 -0500

    name and pass added

--- training_core/src/org/musclemen/model/usermgmt/User.java
+++ training_core/src/org/musclemen/model/usermgmt/User.java
@@ -1,5 +1,21 @@
 package org.musclemen.model.usermgmt;
 
-public class User {
-
+public class User 
+{
+	private String name;
+	private String password;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 }
