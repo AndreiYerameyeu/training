f63135748575748801f02779b8b8bcbbeac6e588
AndreiYerameyeu <Andrei.Yerameyeu@gmail.com>
2012-07-05 09:27:24 +1200

    Added JPA Entity configurations to Set and Workout

--- training_core/src/org/musclemen/model/training/Set.java
+++ training_core/src/org/musclemen/model/training/Set.java
@@ -2,6 +2,14 @@ package org.musclemen.model.training;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
 /**
  * The <code>Set</code> is responsible for keeping information abou one training set. 
  * It should include the exercise, weight(optional), count (mandatory).
@@ -9,19 +17,25 @@ import java.math.BigDecimal;
  * @author Andrei_Yerameyeu
  *
  */
+@Entity
 public class Set {
 	
+	@Column(name="ID")
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE)
 	private Integer id;
 	
-	
+	@OneToOne
+	@JoinColumn(name="EXERCISE_ID")
 	private Exercise exercise;
 	
 	/*
 	 * BigDecimal should be enough to store it :)
 	 */
+	@Column(name="WEIGHT")
 	private BigDecimal weigh;
 	
-	
+	@Column(name="COUNT")
 	private Integer count;
 
 
--- training_core/src/org/musclemen/model/training/Workout.java
+++ training_core/src/org/musclemen/model/training/Workout.java
@@ -4,18 +4,39 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 /**
  * Several Sets that should be/ were executed on a particular day.
  * 
  * @author Andrei_Yerameyeu
  *
  */
+@Entity
 public class Workout {
 	
+	@Column(name="ID")
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE)
 	private Integer id;
 	
+	@OneToMany
+	@JoinTable(	name ="WORKOUT_SETS", 
+				joinColumns = @JoinColumn(name="WRKT_ID"),
+				inverseJoinColumns =  @JoinColumn(name="SET_ID"))
 	private List<Set> sets = new ArrayList<Set>();
 	
+	@Column(name="DATE")
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date date;
 
 	public Integer getId() {
